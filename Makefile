732b2c3c07dd0504dc00ab21ff0ef5ab53322830
宇宙广宇 <heguangyu5@qq.com>
2015-06-15 18:26:11 +0800

    read sectors by CHS ok

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
 all:
 	as -o boot.o boot.s
-	ld --oformat binary -o boot boot.o
+	ld --oformat binary -N -Ttext=0x7c00 -o boot boot.o
 clean:
-	rm boot.o boot
+	rm boot.o boot *.img
 floppy:
-	mkfs.msdos -C floppy.img 1440
-run:
+	bximage -fd -size=1.44 -q floppy.img
+	dd if=800-chars.txt of=floppy.img seek=1 count=2 conv=notrunc
+disk:
+	bximage -hd  -mode=flat -size=10 -q disk.img
+	dd if=800-chars.txt of=disk.img seek=1 count=2 conv=notrunc
+run-floppy:
 	dd if=boot of=floppy.img count=1 conv=notrunc		
-	bochs
+	bochs 'boot:floppy'
+run-disk:
+	dd if=boot of=disk.img count=1 conv=notrunc
+	bochs 'boot:disk'
+
--- boot.s
+++ boot.s
@@ -1,7 +1,81 @@
+.code16
 .section .text
-.global _start;
+.global _start
 _start:
-	nop
+	movb $0x00, %ah
+	movb $0x0e, %al
+	int $0x10
+
+	movw $msg_check_ext, %si
+	call print_msg
+	movb $0x41, %ah
+	movw $0x55aa, %bx
+	int $0x13
+	jc ext_not_present
+	cmpw $0xaa55, %bx
+	je ext_present
+	jmp ext_not_present
+
+ext_not_present:
+	movw  $msg_ext_not_present, %si
+	call print_msg
+	call reset_disk
+	xorw %ax, %ax
+	movw %ax, %es
+	movw $0x7e01, %bx # 0x7c00 + 512 + 1
+	movb $0x02, %ah
+	movb $2, %al
+	movb $0, %ch
+	movb $2, %cl
+	movb $0, %dh
+	int $0x13
+	cmpb $0, %ah
+	jne error
+	movw $0x7e01, %si
+	call print_msg
+	jmp end
+
+ext_present:
+	movw $msg_ext_present, %si
+	call print_msg
+	call reset_disk
+
+reset_disk:
+	movb $0x00, %ah
+	int $0x13
+	jc error
+	ret
+
+error:
+	movw $msg_error, %si
+	call print_msg
+	jmp end
+
+print_msg:
+	movb $0x0e, %ah
+	movb $0x05, %bl
+	print_char:
+		lodsb
+		cmpb $0, %al
+		je print_msg_end
+		int $0x10
+		jmp print_char
+	print_msg_end:
+		ret
+
+msg_check_ext:
+	.string "check ext\r\n"
+msg_ext_not_present:
+	.string "ext not present, use CHS read\r\n"
+msg_ext_present:
+	.string "ext present, use LBA read\r\n"
+msg_error:
+	.string "error occured"
+
+end:
+	hlt
+
+
 	.org 510
 	.byte 0x55
 	.byte 0xaa
