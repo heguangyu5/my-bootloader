9e1b180ab64dbaaeb6bb5cf2b44cd781befdf6cd
宇宙广宇 <heguangyu5@qq.com>
2015-06-17 22:29:33 +0800

    提交保存一下

--- stage1.s
+++ stage1.s
@@ -49,10 +49,48 @@ readSectors:
 	readStart:
 		movw $5, %di	# 5 retries for error
 	sectorLoop:
-		push %ax
-		push %bx
-		push %cx
+		pushw %ax
+		pushw %bx
+		pushw %cx
 		call LBACHS
+		movb $0x02, %ah
+		movb $0x01, %al
+		movb absoluteTrack, %ch
+		movb absoluteSector, %cl
+		movb absoluteHead, %dh
+		movb bsDriveNumber, %dl
+		int $0x13
+		jnc success
+		xorw %ax, %ax
+		int $0x13
+		decw %di
+		popw %cx
+		popw %bx
+		popw %ax
+		jnz sectorLoop
+		int $0x18
+	success:
+		movw msgProgress, %si
+		call print
+		popw %cx
+		popw %bx
+		popw %ax
+		addw bpbBytesPerSector, %bx
+		incw %ax
+		loop readStart
+		ret
+
+/**
+ * convert CHS to LBA
+ * LBA = (cluster - 2) * sectors per cluster
+ */
+clusterLBA:
+	subw $2, %ax
+	xorw %cx, %cx
+	movb bpbSectorsPerCluster, %cl
+	mulw %cx
+	addw datasector, %ax
+	ret
 
 /**
  * convert LBA to CHS
@@ -63,30 +101,151 @@ readSectors:
  */
 LBACHS:
 	xorw %dx, %dx
-	divw $bpbSectorsPerTrack
+	divw bpbSectorsPerTrack
+	incb %dl
+	movb %dl, absoluteSector
+	xorw %dx, %dx
+	divw bpbHeadsPerCylinder
+	movb %dl, absoluteHead
+	movb %al, absoluteTrack
+	ret
 
 /* Bootloader Entry Point */
 main:
-
-.reset:
-	movb $0x00, %ah
-	movb $0x00, %dl
-	int $0x13
-	jc .reset
-
-	movw $0x1000, %ax
+	
+	cli
+	movw $0x07C0, %ax
+	movw %ax, %ds
 	movw %ax, %es
-	xorw %bx, %bx  # %es:%bx = buffer, 0x1000:0 = 64K, this is the esp, is it OK?
+	movw %ax, %fs
+	movw %ax, %gs
 
-	movb $0x02, %ah # read
-	movb $1, %al 	# read 1 sector
-	movb $1, %ch	# track 1
-	movb $2, %cl	# read the 2th secotr
-	movb $0, %dh	# head 0
-	movb $0, %dl	# drive number, floppy is 0
-	int $0x13
+	/* create stack */
+	movw $0x0000, %ax
+	movw %ax, %ss
+	movw $0xFFFF, %sp # 64K
+	sti
 
-	jmp 0x10000
+	/* print loading msg */
+	movw $msgLoading, %si
+	call print
+
+	/* load root direcotry table */
+	loadRoot:
+		xorw %cx, %cx
+		xorw %dx, %dx
+		movw $32, %ax
+		mulw bpbRootEntries
+		divw bpbBytesPerSector
+		xchgw %cx, %ax				# %cx (root dir table sectors) now should be 14 = 32B * 224 / 512B
+		
+		movb bpbNumberOfFATs, %al
+		mulb bpbSectorsPerFAT
+		addw bpbReservedSectors, %ax
+		movw %ax, datasector
+		addw %cx, datasector
+		
+		# read root dir table into memory (0x07C0:0200)
+		movw $0x0200, %bx
+		call readSectors
+	
+	/* find stage 2 */
+		movw bpbRootEntries, %cx
+		movw $0x0200, %di
+		findStart:
+			pushw %cx
+			movw $11, %cx
+			movw imageName, %si
+			pushw %di
+			rep cmpsb
+			popw %di
+			je loadFAT
+			popw %cx
+			addw $32, %di
+			loop findStart
+			jmp FAILURE
+			
+	/* load FAT */
+	loadFAT:
+		movw $msgCRLF, %si
+		call print
+		movw 26(%di), %dx
+		movw %dx, cluster
+
+		xorw %ax, %ax
+		movb bpbNumberOfFATs, %al
+		mulw bpbSectorsPerFAT
+		movw %ax, %cx
+
+		movw bpbReservedSectors, %ax
+
+		# read FAT into memory (0x07C0:0200)
+		movw $0x0200, %bx
+		call readSectors
+
+		# read image file into memory (0x0500:0000)
+		movw $msgCRLF, %si
+		call print
+		movw $0x0050, %ax
+		movw %ax, %es
+		xorw %bx, %bx
+		pushw %bx
+
+	/* load stage 2 */
+	loadImage:
+		movw cluster, %ax
+		popw %bx
+		call clusterLBA
+		xorw %cx, %cx
+		movb bpbSectorsPerCluster, %cl
+		call readSectors
+		pushw %bx
+
+		# next cluster
+		movw cluster, %ax
+		movw %ax, %cx
+		movw %ax, %dx
+		shr $1, %dx
+		add %dx, %cx
+		movw $0x0200, %bx
+		addw %cx, %bx
+		movw (%bx), %dx
+		test $1, %ax
+		jnz oddCluster
+	evenCluster:
+		andw $0b0000111111111111, %dx
+		jmp done
+	oddCluster:
+		shr $4, %dx
+	done:
+		movw %dx, cluster
+		cmpw $0x0FF0, %dx
+		jb loadImage
+	
+		movw $msgCRLF, %si
+		call print
+		pushw $0x0050
+		pushw $0x0000
+		retf
+		
+	FAILURE:
+		movw $msgFailure, %si
+		call print
+		xorb %ah, %ah
+		int $0x16		# await keypress
+		int $0x19		# warm boot computer
+
+absoluteSector: .byte 0
+absoluteHead:	.byte 0
+absoluteTrack:	.byte 0
+
+datasector: 	.word 0
+cluster:		.word 0
+imageName:		.ascii "KRNLDR  SYS"
+msgLoading:		.string "\r\nLoading Boot Image\r\n"
+msgCRLF: 		.string "\r\n"
+msgProgress: 	.string "."
+msgFailure: 	.string "\r\nError: Press Any Key to Reboot\r\n"
 	
 	.org 510
 	.word 0xaa55
